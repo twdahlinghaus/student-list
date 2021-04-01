68b085b6fdfaced078804a90c3f262d85f8e70fe
Thomas Dahlinghaus <twdahlinghaus@gmail.com>
2021-03-31 20:32:42 -0400

    angular practice 3.30.21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Student List</h1>',
+  styles: ['h1 { color: red }']
 })
 export class AppComponent {
   title = 'student-list';
